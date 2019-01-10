cf7356da55b4878b2aa539d231deacd3665b07b2
Maris <maris@tammik.ca>
2019-01-10 15:20:24 +1300

    added sliders to volume and pitch randomization

--- Assets/Scripts/AudioAsset.cs
+++ Assets/Scripts/AudioAsset.cs
@@ -4,9 +4,13 @@
 public class AudioAsset : ScriptableObject
 {
     public AudioClip Clip;
+    [Range(-24, 24)]
     public float PitchBase;
+    [Range(0, 24)]
     public float PitchOffset;
+    [Range(-80, 0)]
     public float VolumeBase;
+    [Range(0, 20)]
     public float VolumeOffset;
 
     public void Play(){
