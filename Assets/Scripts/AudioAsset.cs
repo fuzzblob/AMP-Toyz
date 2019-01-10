7f904262ac372775bb53d412f698163a293dcd2a
Maris <maris@tammik.ca>
2019-01-10 13:07:08 +1300

    added simple playback

--- Assets/Scripts/AudioAsset.cs
+++ Assets/Scripts/AudioAsset.cs
@@ -3,5 +3,14 @@
 [CreateAssetMenu(fileName = "AudioAsset", menuName = "Audio/Create Asset")]
 public class AudioAsset : ScriptableObject
 {
-    
+    public AudioClip clip;
+
+    public void Play(){
+        var go = new GameObject("Source");
+        var s = go.AddComponent<AudioSource>();
+        s.clip = clip;
+        s.spatialBlend = 0f;
+        
+        s.Play();
+    }
 }
